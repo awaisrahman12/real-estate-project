dd81044872419c653c0b4eef23ba9a8e4882329f
awaisrahman12 <awaisrahman011@gmail.com>
2024-05-21 09:13:42 +0500

    Create error handling & Sign up page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../Models/user.model.js";
 import bcyrpy from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res)=>{
+export const signup = async (req, res, next)=>{
 
 const { username, email, password} = req.body;
 const hashedPassword = bcyrpy.hashSync(password, 10);
@@ -9,7 +10,7 @@ const newUser = new User({username, email, password: hashedPassword});
 try {
     await newUser.save()
     res.status(201).json("User create successfully!");
-} catch(err){
-    res.status(500).json(err.message);
+} catch(error){
+    next(error);
     }
 };
